60665291ffc09cce1e295b61529b1d1043b7a908
Joan Gomez <codigo.abierto.p@gmail.com>
2026-02-18 10:59:37 -0500

    Feature de persistencia de los datos consultados de los activos y configuracion de apikey en docker para uso correcto del servicio

--- etl-service/src/test/java/com/financial/etl/integration/TwelveDataHttpClientIT.java
+++ etl-service/src/test/java/com/financial/etl/integration/TwelveDataHttpClientIT.java
@@ -1,10 +1,12 @@
 package com.financial.etl.integration;
 
 import com.financial.etl.client.TwelveDataHttpClient;
+import com.financial.etl.repository.FinancialSeriesRepository;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -26,6 +28,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 )
 @Tag("integration")
 class TwelveDataHttpClientIT {
+
+    @MockBean
+    private FinancialSeriesRepository financialSeriesRepository;
 
     @Autowired
     private TwelveDataHttpClient client;
